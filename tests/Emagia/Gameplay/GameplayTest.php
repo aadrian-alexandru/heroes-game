a71c6f4095d29cb924ed23f6fd72f115bbb0a0a8
Adrian-Alexandru Alecu <aalecu@tremend.ro>
2018-02-05 23:32:43 +0200

    - added test class for Gameplay.

---
+++ tests/Emagia/Gameplay/GameplayTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Adrian
+ * Date: 05-Feb-18
+ * Time: 22:39
+ */
+
+namespace Tests\Emagia\Gameplay;
+
+use PHPUnit\Framework\TestCase;
+
+class GameplayTest extends TestCase
+{
+
+}
