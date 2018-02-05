7567b1e834febaf6a7fad104fd6f0c6fc11a9471
Adrian-Alexandru Alecu <aalecu@tremend.ro>
2018-02-05 22:17:13 +0200

    - adding unit tests for Character class.

--- tests/Emagia/Character/CharacterTest.php
+++ tests/Emagia/Character/CharacterTest.php
@@ -20,6 +20,9 @@ class CharacterTest extends TestCase
     /** @var Character */
     private $character;
 
+    /**
+     * Set up
+     */
     public function setUp()
     {
         $this->character = new Character(self::HERO_NAME);
@@ -31,17 +34,67 @@ class CharacterTest extends TestCase
             ->setLuck(50);
     }
 
-    public function testGetHealth()
+    /**
+     * Test Get Name
+     */
+    public function testGetName()
     {
         $character = $this->character;
-        self::assertEquals(10, $character->getHealth());
+        self::assertEquals(self::HERO_NAME, $character->getName());
     }
 
+    /**
+     * Test Set and Get Health
+     */
     public function testSetHealth()
     {
         $character = $this->character;
         self::assertEquals(10, $character->getHealth());
         $character->setHealth(40);
         self::assertEquals(40, $character->getHealth());
+    }
+
+    /**
+     * Test Set and Get Strength
+     */
+    public function testSetStrength()
+    {
+        $character = $this->character;
+        self::assertEquals(20, $character->getStrength());
+        $character->setStrength(35);
+        self::assertEquals(35, $character->getStrength());
+    }
+
+    /**
+     * Test Set and Get Defense
+     */
+    public function testSetDefense()
+    {
+        $character = $this->character;
+        self::assertEquals(30, $character->getDefense());
+        $character->setDefense(52);
+        self::assertEquals(52, $character->getDefense());
+    }
+
+    /**
+     * Test Set and Get Speed
+     */
+    public function testSetSpeed()
+    {
+        $character = $this->character;
+        self::assertEquals(40, $character->getSpeed());
+        $character->setSpeed(18);
+        self::assertEquals(18, $character->getSpeed());
+    }
+
+    /**
+     * Test Set and Get Luck
+     */
+    public function testSetLuck()
+    {
+        $character = $this->character;
+        self::assertEquals(50, $character->getLuck());
+        $character->setLuck(72);
+        self::assertEquals(72, $character->getLuck());
     }
 }
